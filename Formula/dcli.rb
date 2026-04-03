1bc7decb3545f43abce088827a941ed8a85d1356
Oleg Koval <kvl.olg@gmail.com>
2026-04-03 11:45:17 +0200

    chore: update SHA256 checksums for v0.1.0 release

--- Formula/dcli.rb
+++ Formula/dcli.rb
@@ -6,26 +6,22 @@ class Dcli < Formula
   on_macos do
     on_arm do
       url "https://github.com/oleg-koval/dcli/releases/download/v0.1.0/dcli-darwin-arm64.tar.gz"
-      # SHA256 will be updated on release
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "138ed5cc840286c4040e2c5b77f038bf3cb8b16c89c4b5030d289ac74e5fb135"
     end
     on_intel do
       url "https://github.com/oleg-koval/dcli/releases/download/v0.1.0/dcli-darwin-amd64.tar.gz"
-      # SHA256 will be updated on release
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "bb77c28361e2fe289fcf7d83723d05791a4d670548d59a923fc99b983ff9a1c5"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/oleg-koval/dcli/releases/download/v0.1.0/dcli-linux-amd64.tar.gz"
-      # SHA256 will be updated on release
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "6001b60b6b6dcec242a90bb54c0cb37a95d11b728f0b053f26c6d3fd1ac9b640"
     end
     on_arm do
       url "https://github.com/oleg-koval/dcli/releases/download/v0.1.0/dcli-linux-arm64.tar.gz"
-      # SHA256 will be updated on release
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "3651e008a54259b544c387febd206ff147c294b44faf16927bdb43eb0e7e85cf"
     end
   end
 
